8f3e6e8f387783d14340f06a8f1469be189d07d4
Burak <burakceylann@outlook.com>
2022-03-28 12:08:07 +0300

    Add image api fragment

---
+++ app/src/main/java/burak/ceylan/artbookwithunittest/view/ImageApiFragment.kt
@@ -0,0 +1,14 @@
+package burak.ceylan.artbookwithunittest.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import burak.ceylan.artbookwithunittest.R
+
+class ImageApiFragment : Fragment(R.layout.fragment_image_api) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
