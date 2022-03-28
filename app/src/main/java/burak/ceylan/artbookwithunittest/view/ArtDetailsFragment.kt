b7e9a7647ef3c5285e49e03d8eb96a7fef2f0c28
Burak <burakceylann@outlook.com>
2022-03-28 12:07:53 +0300

    Add art details fragment

---
+++ app/src/main/java/burak/ceylan/artbookwithunittest/view/ArtDetailsFragment.kt
@@ -0,0 +1,14 @@
+package burak.ceylan.artbookwithunittest.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import burak.ceylan.artbookwithunittest.R
+
+class ArtDetailsFragment : Fragment(R.layout.fragment_art_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
