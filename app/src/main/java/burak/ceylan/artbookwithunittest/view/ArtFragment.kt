002e27d581d30fb8fc324ef525513494fd28d1e9
Burak <burakceylann@outlook.com>
2022-03-28 16:11:47 +0300

    Add art fragment viewbinding and navigation

--- app/src/main/java/burak/ceylan/artbookwithunittest/view/ArtFragment.kt
+++ app/src/main/java/burak/ceylan/artbookwithunittest/view/ArtFragment.kt
@@ -3,13 +3,28 @@ package burak.ceylan.artbookwithunittest.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import burak.ceylan.artbookwithunittest.R
+import burak.ceylan.artbookwithunittest.databinding.FragmentArtsBinding
 
 class ArtFragment : Fragment(R.layout.fragment_arts) {
+
+    private var fragmentBinding : FragmentArtsBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val binding = FragmentArtsBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
+        }
+    }
+
+    override fun onDestroy() {
+        fragmentBinding = null
+        super.onDestroy()
     }
 
 }
