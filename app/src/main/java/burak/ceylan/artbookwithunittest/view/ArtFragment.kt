b3c998b93e1ee4af7fb424fd30b559e12e38fee8
Burak <burakceylann@outlook.com>
2022-03-28 12:05:00 +0300

    Add art fragment

---
+++ app/src/main/java/burak/ceylan/artbookwithunittest/view/ArtFragment.kt
@@ -0,0 +1,15 @@
+package burak.ceylan.artbookwithunittest.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import burak.ceylan.artbookwithunittest.R
+
+class ArtFragment : Fragment(R.layout.fragment_arts) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
