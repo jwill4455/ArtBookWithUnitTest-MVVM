6e62c00f53df72f2545c97ad501b78cf6c7f7a64
Burak <burakceylann@outlook.com>
2022-03-28 16:16:36 +0300

    add logic for backpress to previous screen

--- app/src/main/java/burak/ceylan/artbookwithunittest/view/ArtDetailsFragment.kt
+++ app/src/main/java/burak/ceylan/artbookwithunittest/view/ArtDetailsFragment.kt
@@ -2,6 +2,7 @@ package burak.ceylan.artbookwithunittest.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import burak.ceylan.artbookwithunittest.R
@@ -19,6 +20,13 @@ class ArtDetailsFragment : Fragment(R.layout.fragment_art_details) {
 
         binding.artImageView.setOnClickListener {
             findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
+
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+
         }
     }
 
